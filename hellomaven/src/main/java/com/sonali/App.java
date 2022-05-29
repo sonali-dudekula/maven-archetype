153654b24f358e3a8005bc73208d22ab658c42d0
Sai Sonali <sonalidudekula@gmail.com>
2022-05-29 22:09:08 +0530

    Add files via upload

---
+++ hellomaven/src/main/java/com/sonali/App.java
@@ -0,0 +1,13 @@
+package com.sonali;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sumofnum/src/main/java/com/sonali/App.java
@@ -0,0 +1,17 @@
+package com.sonali;
+import java.util.*;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a+b;
+        System.out.println(c);
+    }
+}
